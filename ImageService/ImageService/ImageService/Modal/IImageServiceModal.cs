fa474e916f225fc0bbbb2d3ec87900c3139c4665
ovednagar <33989713+ovednagar@users.noreply.github.com>
2018-04-02 13:30:26 +0300

    first Upload

---
+++ ImageService/ImageService/ImageService/Modal/IImageServiceModal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Modal
+{
+    public interface IImageServiceModal
+    {
+        /// <summary>
+        /// The Function Addes A file to the system
+        /// </summary>
+        /// <param name="path">The Path of the Image from the file</param>
+        /// <returns>Indication if the Addition Was Successful</returns>
+        void AddFile(string path, out bool result);
+        void RenameFile(string path, string NewName, out bool result);
+        void CreateFolder(string path, out bool result);
+        void DeleteFile(string path, out bool result);
+        void DeleteFolder(string path, out bool result);
+    }
+}
